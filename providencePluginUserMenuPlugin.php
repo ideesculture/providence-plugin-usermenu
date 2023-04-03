c1eccc2e4b53bdc47050a5164310c8beb24c1b9d
GautierMichelin <gautiermichelin@users.noreply.github.com>
2023-04-03 18:08:29 +0200

    Update providencePluginUserMenuPlugin.php

--- providencePluginUserMenuPlugin.php
+++ providencePluginUserMenuPlugin.php
@@ -1,6 +1,6 @@
 <?php
 /* ----------------------------------------------------------------------
- * mediaImportPlugin.php :
+ * providencePluginUserMenuPlugin.php :
  * ----------------------------------------------------------------------
  * CollectiveAccess
  * Open-source collections management software
@@ -9,23 +9,10 @@
  * Software by Whirl-i-Gig (http://www.whirl-i-gig.com)
  * Copyright 2010 Whirl-i-Gig
  *
- * For more information visit http://www.CollectiveAccess.org
- *
- * This program is free software; you may redistribute it and/or modify it under
- * the terms of the provided license as published by Whirl-i-Gig
- *
- * CollectiveAccess is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTIES whatsoever, including any implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * This source code is free and modifiable under the terms of
- * GNU General Public License. (http://www.gnu.org/copyleft/gpl.html). See
- * the "license.txt" file for details, or visit the CollectiveAccess web site at
- * http://www.CollectiveAccess.org
+ * This plugin : IdéesCulture 2015-2023 www.ideesculture.com
  *
  * ----------------------------------------------------------------------
  */
-
 	class providencePluginUserMenuPlugin extends BaseApplicationPlugin {
 		# -------------------------------------------------------
 		protected $description = 'Plugin for CollectiveAccess moving the content of the downside bar to a button';
@@ -59,7 +46,19 @@
 			if ($o_req = $this->getRequest()) {
 
                 //we define the content of a new main menu item
-				$va_menu_items = $pa_menu_bar['manage']['navigation'];
+				if (isset($pa_menu_bar['providencePluginUserMenu_menu'])) {
+					$va_menu_items = $pa_menu_bar['providencePluginUserMenu_menu']['navigation'];
+					if (!is_array($va_menu_items)) { $va_menu_items = array(); }
+				} else {
+					$va_menu_items = array();
+				}
+
+				$va_menu_items[Mes_preferences] = array(
+						'displayName' => 'Mes préférences',
+						"default" => array(
+							'action' => '/system/Preferences/EditUIPrefs'
+						)
+					);
 
 				$va_menu_items[Deconnexion] = array(
 						'displayName' => 'Déconnexion',
@@ -67,11 +66,20 @@
 							'action' => '/system/auth/logout'
 						)
 					);
+				//this adds the CSS changing the style and content of the footer
+				MetaTagManager::addLink('stylesheet', __CA_URL_ROOT__."/app/plugins/providencePluginUserMenu/css/providencePluginUserMenu.css",'text/css');
+				if($this->opo_config->get('footer') == "1") {
+					MetaTagManager::addLink('stylesheet', __CA_URL_ROOT__."/app/plugins/providencePluginUserMenu/css/providencePluginUserMenuFooter.css",'text/css');
+				}
+				
+				
+				
+				print $va_views_images_path;
 
-                //this adds the CSS changing the style and content of the footer
-                MetaTagManager::addLink('stylesheet', __CA_URL_ROOT__."/app/plugins/providencePluginUserMenu/css/providencePluginUserMenu.css",'text/css');
-
-				$pa_menu_bar['manage']['navigation'] = $va_menu_items;
+				$pa_menu_bar['providencePluginUserMenu_menu'] = array(
+					'displayName' => "<img src='https://micg.ideesculture.fr/gestion/themes/default/graphics/icon-on-off.png' style='height: 14px'>",
+					'navigation' => $va_menu_items
+				);
 			}
 
 			return $pa_menu_bar;
